dbca104191c576c0da93dc1d4ae9d68a81ff4d1a
Mohammed <admin@30ac9d0a-2d8b-4ea5-9990-b44f9640e5c2.fritz.box>
2025-03-01 04:11:24 +0100

    fixing favorites

--- app/(mainLayout)/favorites/page.tsx
+++ app/(mainLayout)/favorites/page.tsx
@@ -1,56 +1,59 @@
-import { prisma } from "@/app/utils/db"
+import { prisma } from "@/app/utils/db";
 import { requireUser } from "@/app/utils/requireUser";
 import { EmptyState } from "@/components/utils/EmptyState";
 import JobCard from "@/components/job/JobCard";
 
-async function getFavorites(userId: string){
-    const data = await prisma.savedJobPost.findMany({
-        where: {
-            userId: userId
-        },
+async function getFavorites(userId: string) {
+  const data = await prisma.savedJobPost.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      JobPost: {
         select: {
-            JobPost: {
-                select: {
-                    id: true,
-                    jobTitle: true,
-                    employmentType: true,
-                    createdAt: true,
-                    salaryFrom: true,
-                    location: true,
-                    listingDuration: true,
-                    salaryTo: true,
-                    Company : {
-                        select: {
-                            name: true,
-                            location: true,
-                            about: true,
-                            logo: true,
-                        }
-                    }
-                }
-            }
-        }
-    });
+          id: true,
+          jobTitle: true,
+          salaryFrom: true,
+          salaryTo: true,
+          employmentType: true,
+          location: true,
+          createdAt: true,
+          Company: {
+            select: {
+              name: true,
+              logo: true,
+              location: true,
+              about: true,
+            },
+          },
+        },
+      },
+    },
+  });
 
-    return data;
+  return data;
 }
 
-export default async function FavoritesPage(){
+export default async function FavoritesPage() {
+  const session = await requireUser();
+  const data = await getFavorites(session?.id as string);
 
-    const session = await requireUser();
-    const data = await getFavorites(session?.id as string);
-
-
-    if(data.length === 0){
-        return (
-            <EmptyState title="No Favorites found" description="You don't have any favorites yet." buttonText="Find a job" href="/" />
-        )
-    }
+  if (data.length === 0) {
     return (
-        <div className="grid grid-cols-1 mt-5 gap-4">
-            {data.map((favorite) =>(
-                <JobCard key={favorite.JobPost.id} job={favorite.JobPost} />
-            ))}
-        </div>
+      <EmptyState
+        title="No Favorites found"
+        description="You dont have any favorites yet."
+        buttonText="Find a job"
+        href="/"
+      />
     );
+  }
+
+  return (
+    <div className="grid grid-cols-1 mt-5 gap-4">
+      {data.map((favorite) => (
+        <JobCard key={favorite.JobPost.id} job={favorite.JobPost} />
+      ))}
+    </div>
+  );
 }
