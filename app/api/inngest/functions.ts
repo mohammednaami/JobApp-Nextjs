e901e3b568374073130fdd714a01f71a29741118
Mohammed <admin@30ac9d0a-2d8b-4ea5-9990-b44f9640e5c2.fritz.box>
2025-03-01 04:30:06 +0100

    fixing alles

--- app/api/inngest/functions.ts
+++ app/api/inngest/functions.ts
@@ -14,6 +14,14 @@ type JobWithCompany = {
     };
   };
 
+  export const helloWorld = inngest.createFunction(
+    { id: "hello-world" },
+    { event: "test/hello.world" },
+    async ({ event, step }) => {
+        await step.sleep("wait-a-moment", "1s");
+        return { message: `Hello ${event.data.email}!` };
+    },
+);
 
 
 export const handleJobExpiration = inngest.createFunction(
